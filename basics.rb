9bc2461748ba928a727174c03c5fb5ab975a20e1
FATESAIKOU <qzsecftbhhhh@gmail.com>
2025-12-07 16:19:51 +0900

    Add branches to basic.rb

--- basics.rb
+++ basics.rb
@@ -22,3 +22,33 @@ puts user[age_symbol]
 puts "<<<<<<>>>>>>"
 
 puts '########## Step test variables - End ##########'
+
+# Branches
+
+puts '########## Step test branches - Start ##########'
+if age >= 18
+    puts "[if-else] #{name} is an adult."
+else
+    puts "[if-else] #{name} is a minor."
+end
+
+unless age < 18
+    puts "[unless] #{name} is an adult."
+else
+    puts "[unless] #{name} is a minor."
+end
+
+puts "[ternary] #{name} is " + (age >= 18 ? "an adult." : "a minor.")
+puts "[Single line if] #{name} is an adult." if age >= 18
+puts "[Single line unless] #{name} is a minor." unless age < 18
+
+case age
+when 0..12
+    puts "[case] #{name} is a child."
+when 13..19
+    puts "[case] #{name} is a teenager."
+else
+    puts "[case] #{name} is an adult."
+end
+
+puts '########## Step test branches - End ##########'
